761cdf07d57284493cdb6b4168e683c80ece4b08
Prajakta2307 <prajaktaj3168@gmail.com>
2024-03-18 17:56:50 +0530

    updated project

--- src/test/java/Basic/OrganizationsTest.java
+++ src/test/java/Basic/OrganizationsTest.java
@@ -1,15 +1,54 @@
 package Basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import commonUtils.PropertyFileUtil;
+import commonUtils.WebDriverUtil;
+
 public class OrganizationsTest {
 	
-	@Test
-	public void CreateOrganizationsTest() {
-		WebDriver d=new ChromeDriver();
-		d.get("http://localhost:8888/");
+		
+		
+	
+
+			@Test
+			public void organizations() throws IOException, InterruptedException {
+//				
+//				PropertyFileUtil putil=new PropertyFileUtil();
+//				WebDriverUtil wutil=new WebDriverUtil();
+				WebDriver d=new ChromeDriver();
+				d.get("http://localhost:8888/");
+				
+				//to read data from property file
+//				String URL = putil.getDataFromPropertyFile("Url");
+//				String USERNAME= putil.getDataFromPropertyFile("Username");
+//				String PASSWORD= putil.getDataFromPropertyFile("Password");
+//			
+//				//to launch the Application
+//				d.get(URL);
+//				
+//				//to maximize the window
+//				wutil.Maximize(d);
+//				
+//				//to apply wait for findelement method
+//				wutil.implicitwait(d);
+//				
+//				
+//				//to read from excel sheet
+//				Thread.sleep(2000);
+//				
+//				
+//				
+			
+				//to login to Application
+				d.findElement(By.name("user_name")).sendKeys("admin");
+				d.findElement(By.name("user_password")).sendKeys("admin");
+				d.findElement(By.id("submitButton")).click();
 	}
 
 }
